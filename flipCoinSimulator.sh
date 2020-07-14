38c426929c4777988a2d2ae5a66e9959020a9c78
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-14 12:37:53 +0530

    Multiple times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,6 +9,25 @@ else
         echo "Tails"
 fi
 }
-res=$(coin)
-echo $res
+heads=0
+tails=0
+while [ true ]
+do
+	res=$(coin)
+	echo $res
+	if [ $res == "Heads" ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+	read -p "Do you want to continue?if yes enter 1 else enter 0: " choice
+	if [ $choice -eq 0 ]
+	then
+		echo "No.of times won by heads: "$heads
+		echo "No.of times won by tails: "$tails
+		break
+	fi
+done
+
 
