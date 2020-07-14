a0071098680e7124c61816b365679a04a5afdb70
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-14 12:49:14 +0530

    Repeat flipping till either of them wins 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -11,23 +11,29 @@ fi
 }
 heads=0
 tails=0
-while [ true ]
+while(( $heads<21 && $tails<21))
 do
-	res=$(coin)
-	echo $res
-	if [ $res == "Heads" ]
-	then
-		((heads++))
-	else
-		((tails++))
-	fi
-	read -p "Do you want to continue?if yes enter 1 else enter 0: " choice
-	if [ $choice -eq 0 ]
-	then
-		echo "No.of times won by heads: "$heads
-		echo "No.of times won by tails: "$tails
-		break
-	fi
+        res=$(coin)
+        echo $res
+        if [ $res == "Heads" ]
+        then
+                ((heads++))
+        else
+                ((tails++))
+        fi
 done
+echo "No.of times won by heads: " $heads
+echo "No.of times won by tails: " $tails
+if [ $heads -gt $tails ]
+then
+        diff=$(($heads-$tails))
+        echo "Heads is the winner by "$diff" flips"
+elif [ $heads -lt $tails ]
+then
+        diff=$(($tails-$heads))
+        echo "Tails is the winner by "$diff" flips"
+else
+        echo "It's a tie"
+fi
 
 
